a1eaa825d536e5505b313a468207d458f82bca8a
巫北辰 <1204605317@qq.com>
2018-05-25 20:18:07 +0800

    wozuolsha

--- api/app/Providers/AppServiceProvider.php
+++ api/app/Providers/AppServiceProvider.php
@@ -12,6 +12,11 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
+    public function boot()
+    {
+        Schema::defaultStringLength(191);
+    }
+
     public function register()
     {
         //
