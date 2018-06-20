7520928cd79f38f593d75e5a7ed51ca0741c300f
qiankaihua <qiankaihua@126.com>
2018-06-20 17:17:27 +0800

    Update AlterController.php

--- api/app/Http/Controllers/User/AlterController.php
+++ api/app/Http/Controllers/User/AlterController.php
@@ -27,7 +27,7 @@ class AlterController extends Controller
         if(+$id !== $now_user->id && $role->alias !== 'root') return response(['message'=>'您没有修改该用户的权限'],403);
         $user = \App\Models\User::find($id);
         if($user === null) return response(['message'=>'该用户不存在'],404);
-        if(! app('hash')->check($request->input('password_old'), $user->password)) return response(['message'=>'密码错误'],404);
+        if(! app('hash')->check($request->input('password_old'), $user->password)) return response(['message'=>'密码错误'],444);
         $user->password = $request->input('password_new');
         $user->save();
         return ;
